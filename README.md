2fa973de7a41032ee726927ab9b7e6952b0c53b3
Adam Barnhard <adam.barnhard@vitalsource.com>
2018-10-26 09:27:44 -0500

    starting on adding scripting

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ This project installs and configures Elasticsearch, Mongo, Redis, & Memcached on
 It requires:
 * docker
 
-To use, run `docker-compose up` from the root, then run `docker-compose exec mongo sh -c 'mongo < /data/mongodb/scripts/cluster-init.js && mongo < /data/mongodb/scripts/cluster-config.js'`
+To use, run `docker-compose up` from the root. If you don't want to see the log output, run `docker-compose up --detach` to run images in background
+
+run `docker-compose exec mongo sh -c 'mongo < /data/mongodb/scripts/cluster-init.js && mongo < /data/mongodb/scripts/cluster-config.js'`
 
 That's it. You should now have all services needed to run MLP & Agile Product running locally
---
+++ local.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+start_service="start"
+stop_service="stop"
+build_service="build"
+rebuild_service="rebuild"
+
+if [ -z "$1" ]; then
+  echo 'Error: valid arguments are start, stop, build, rebuild'
+  exit 1
+fi
+
+action="$(echo \"$1\" | awk '{print tolower($0)}')"
+echo "$action"
+if [ "$action" == "$start_service" ]; then
+    echo 'hit 1'
+    docker-compose start;
+elif [ "$action" == "$stop_service" ]; then
+    echo 'hit 2'
+    docker-compose stop;
+elif [ "$action" == "$build_service" ]; then
+    echo 'hit 3'
+    docker-compose up --d && \
+    docker-compose exec mongo sh -c 'mongo < /data/mongodb/scripts/cluster-init.js && mongo < /data/mongodb/scripts/cluster-config.js';
+elif [ $action == $rebuild_service ]; then
+    echo 'hit 4'
+    docker-compose down && \
+    docker-compose up --d && \
+    docker-compose exec mongo sh -c 'mongo < /data/mongodb/scripts/cluster-init.js && mongo < /data/mongodb/scripts/cluster-config.js';
+else
+    echo 'hit 4'
+    echo 'Error: valid arguments are start, stop, build, rebuild';
+fi
